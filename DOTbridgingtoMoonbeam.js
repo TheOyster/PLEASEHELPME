f28e6f8951f32f4c0dbad946290388d9db57d4a1
TheOyster <49369920+TheOyster@users.noreply.github.com>
2023-06-20 18:42:20 +0100

    Create DOTbridgingtoMoonbeam.js

---
+++ DOTbridgingtoMoonbeam.js
@@ -0,0 +1,29 @@
+
+async function depositDOTtoMoonbeam() {
+  const wsProvider = new WsProvider("wss://rpc.polkadot.io");
+  const api = await ApiPromise.create({ provider: wsProvider });
+
+  const mnemonic =
+    "SEED PHRASE ";
+  const keyring = new Keyring({ type: "sr25519" });
+  const polkadotKeyring = keyring.addFromUri(mnemonic);
+
+  const addr = "1CUKKQhuqcbqXSY4QhWZxFct5uTKbWVCMKMUBHqWyc8ds1c";
+
+  const { nonce, data: balance } = await api.query.system.account(addr);
+  const adjustedBalance = balance.free - 22000000000;
+  console.log(` balance of ${adjustedBalance} `);
+
+ 
+
+  const dot = AssetSymbol.DOT;
+  const polkadot = ChainKey.Polkadot;
+  const { chains, from } = moonbeam.deposit(dot);
+
+
+    const { asset, sourceBalance, source, min, send } = await from(polkadot).get("0xD27cEcE5CE8b4f6Fa47A160105D842F97aee227d", polkadotKeyring);
+    await send(+adjustedBalance, (event) => console.log(event));
+ 
+    await send(+"1000000", (event) => console.log(event));
+    await sleep(10000);
+  }
